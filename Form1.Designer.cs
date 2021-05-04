f779aaa4e4f570c669d5e059b39ea2637b84fe5d
Ole <gunnar_holten@hotmail.com>
2021-05-04 16:00:26 +0200

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace FileMatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.hovedFiler = new System.Windows.Forms.Label();
+            this.nyFiler = new System.Windows.Forms.Label();
+            this.filerMangler = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::FileMatcher.Properties.Resources.logopng;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(286, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 93);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(381, 195);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(441, 129);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(388, 195);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 487);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(817, 195);
+            this.listBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(175, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hoved";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(623, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ny";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(355, 464);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Filer som Mangler";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(120)))), ((int)(((byte)(187)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(235, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(388, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Kjør Sjekk";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 361);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "C:\\";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(441, 361);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(388, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "C:\\NyMappe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(9, 718);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Utviklet av Ole Gunnar Holten";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(120)))), ((int)(((byte)(187)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(819, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 33);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hovedFiler
+            // 
+            this.hovedFiler.AutoSize = true;
+            this.hovedFiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hovedFiler.ForeColor = System.Drawing.Color.White;
+            this.hovedFiler.Location = new System.Drawing.Point(175, 327);
+            this.hovedFiler.Name = "hovedFiler";
+            this.hovedFiler.Size = new System.Drawing.Size(0, 20);
+            this.hovedFiler.TabIndex = 13;
+            // 
+            // nyFiler
+            // 
+            this.nyFiler.AutoSize = true;
+            this.nyFiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nyFiler.ForeColor = System.Drawing.Color.White;
+            this.nyFiler.Location = new System.Drawing.Point(623, 327);
+            this.nyFiler.Name = "nyFiler";
+            this.nyFiler.Size = new System.Drawing.Size(0, 20);
+            this.nyFiler.TabIndex = 14;
+            // 
+            // filerMangler
+            // 
+            this.filerMangler.AutoSize = true;
+            this.filerMangler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filerMangler.ForeColor = System.Drawing.Color.White;
+            this.filerMangler.Location = new System.Drawing.Point(393, 685);
+            this.filerMangler.Name = "filerMangler";
+            this.filerMangler.Size = new System.Drawing.Size(0, 20);
+            this.filerMangler.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(144, 384);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Hoved filer Path";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(550, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(194, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Ny Mappe Plassering Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(858, 740);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.filerMangler);
+            this.Controls.Add(this.nyFiler);
+            this.Controls.Add(this.hovedFiler);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "File Matcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label hovedFiler;
+        private System.Windows.Forms.Label nyFiler;
+        private System.Windows.Forms.Label filerMangler;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileMatcher
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * Global vars
+         */
+        string appPath = AppDomain.CurrentDomain.BaseDirectory;
+        FileStream stream = null;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Så vi kan flytte rundt må vinduet våres
+         */
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             * Sjekk om hoved og ny txt filene er laget, vis ikke lager vi de
+             */
+            try
+            {
+                if (!File.Exists(appPath + "\\hoved.txt"))
+                {
+                    stream = File.Create(appPath + "\\hoved.txt");
+                    stream.Close();
+                }
+
+                if (!File.Exists(appPath + "\\ny.txt"))
+                {
+                    stream = File.Create(appPath + "\\ny.txt");
+                    stream.Close();
+                }
+            } catch (Exception er)
+            {
+                MessageBox.Show("Error! \n" + er, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } finally
+            {
+                //Lukke stream
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
+            
+            //Last inn filene i listbox
+            lastFiler();
+        }
+
+        public void lastFiler()
+        {
+            /*
+             * last in alt av informasjon om filene
+             */
+            if (File.Exists(appPath + "\\hoved.txt"))
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                listBox3.Items.Clear();
+
+                int hovedAmount = 0;
+                int nyAmount = 0;
+                int missingAmount = 0;
+
+                /*
+                 * Hent filer fra hoved mappen 
+                 */
+                foreach (string hovedLine in File.ReadLines(appPath + "\\hoved.txt"))
+                {
+                    listBox1.Items.Add(hovedLine);
+                    hovedAmount++;
+                }
+
+                hovedFiler.Text = hovedAmount.ToString();
+
+                /*
+                 * Hent filer fra den nye mappen
+                 */
+                foreach (string nyLine in File.ReadLines(appPath + "\\ny.txt"))
+                {
+                    listBox2.Items.Add(nyLine);
+                    nyAmount++;
+                }
+
+                nyFiler.Text = nyAmount.ToString();
+
+                /*
+                 * Sjekk hvilken filer som ikke matcher mellom hoved og ny
+                 */
+                foreach (string hoved in File.ReadLines(appPath + "\\hoved.txt"))
+                {
+                    if (!File.ReadAllText(appPath + "\\ny.txt").Contains(hoved))
+                    {
+                        listBox3.Items.Add(hoved);
+                        missingAmount++;
+                    }
+                }
+
+                filerMangler.Text = missingAmount.ToString();
+
+            }
+        }
+
+        /*
+         * Kjør sjekk
+         */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*
+                 * Lag txt filene vis det trengs
+                 */
+                if (!File.Exists(appPath + "\\hoved.txt"))
+                {
+                    File.Create(appPath + "\\hoved.txt");
+                }
+
+                if (!File.Exists(appPath + "\\ny.txt"))
+                {
+                    File.Create(appPath + "\\ny.txt");
+                }
+
+                //Gjør hoved.txt empty så vi ikke for gammel data
+                File.WriteAllText(appPath + "\\hoved.txt", "");
+
+                /*
+                 * Skriv inn i hoved txt filen
+                 */
+                if (textBox1.Text != "")
+                {
+                    foreach (string hovedFil in Directory.EnumerateFiles(textBox1.Text, "*.*", SearchOption.AllDirectories))
+                    {
+                        File.AppendAllText(appPath + "\\hoved.txt", "\n" + hovedFil);
+                    }
+                }
+
+                /*
+                 * Skriv inn i ny txt filen
+                 */
+                if (textBox2.Text != "")
+                {
+                    foreach (string nyFil in Directory.EnumerateFiles(textBox2.Text, "*.*", SearchOption.AllDirectories))
+                    {
+                        File.AppendAllText(appPath + "\\ny.txt", "\n" + nyFil);
+                    }
+                }
+
+                lastFiler();
+
+                MessageBox.Show("Jobb gjennomført!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } catch (Exception er)
+            {
+                MessageBox.Show("Feil ved fil sjekk! \nPrøv igjen! \n" + er, "Feil", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /*
+         * Exit knappen
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
